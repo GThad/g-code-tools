586a8f949abc8bb75404c6272f8a4ce42dbce73f
Gifan Thadathil <gifan.thada@gmail.com>
2018-10-04 16:53:51 +0000

    Reimplement G-code importing.
    Before, importing G-code from some input-port was split
    into two step: lexing and parsing. However, this is needlessly
    complicated since G-code itself (syntactically speaking is a
    regular language! We make two simplifications:
    
    1. Implement importing using just the lexer function provided
       by parser-tools. Subsequently remove the "parser".
    2. Remove any notion of tokens. We can create the code structs
       directly in the lexer.
    
    There were two other issues with the previous lexer implementation:
    
    1. Multi-digit, decimal, and negative numbers were not recognized.
       This is due to using an incorrect regular expression.
    2. Letters were not converted from string to symbols as expected.
    
    These two issues are fixed in this commit.

--- gcode-tools.rkt
+++ gcode-tools.rkt
@@ -7,6 +7,7 @@
          racket/list
          racket/struct
          parser-tools/lex
+         (prefix-in re- parser-tools/lex-sre)
          parser-tools/yacc)
 
 ;; -------------------- UTILITY FUNCTIONS/MACROS
@@ -69,66 +70,31 @@
   (or ((make-letter-code-checker 'M) a-code)
       ((make-letter-code-checker 'm) a-code)))
 
-;; -------------------- TOKENS
+;; -------------------- PARSING
 
-;; Defines tokens used during lexing. Produces
-;; functions for creating token structs.
-;;     (token-CODE-LETTER value) -> (token 'CODE-LETTER value)
-;;     (token-CODE-NUMBER value) -> (token 'CODE-NUMBER value)
-;; where value is anything.
-(define-tokens code-tokens (CODE-LETTER CODE-NUMBER))
-(define-empty-tokens empty-tokens (EOF))
+(define-lex-abbrev gcode-letter
+  (re-or #\G #\M #\S #\F #\X #\Y #\Z #\I #\J #\K
+         #\g #\m #\s #\f #\x #\y #\z #\i #\j #\k))
 
-;; Consumes anything and produces #t whenever expr is a
-;; CODE-LETTER token.
-(define (token-CODE-LETTER? expr)
-  (and (token? expr)
-       (symbol=? 'CODE-LETTER (token-name expr))))
+(define-lex-abbrev gcode-number
+  (re-seq (re-? #\- #\+)
+          (re-* numeric)
+          (re-? #\.)
+          (re-+ numeric)))
 
-;; Consumes anything and produces #t whenever expr is a
-;; CODE-NUMBER token.
-(define (token-CODE-NUMBER? expr)
-  (and (token? expr)
-       (symbol=? 'CODE-NUMBER (token-name expr))))
-
-;; Consumes anything and produces #t whenever the argument is
-;; a CODE-LETTER or CODE-NUMBER token.
-(define gcode-token?
-  (or/c token-CODE-LETTER?
-        token-CODE-NUMBER?))
-
-;; -------------------- LEXING
+;; Consume a string that looks like a code? and product the corresponding code?
+(define (code-str->code str)
+  (define split-str
+    (cdr (regexp-match #px"([G|M|S|F|X|Y|Z|I|J|K])([-|+]?\\d*\\.?\\d+)"
+                   (string-replace str #px"\\s" ""))))
+  (code (string->symbol (first split-str))
+        (string->number (second split-str))))
 
 ;; Consumes an input-port?, reads from the input-port exactly
 ;; enough characters to produce a single token.
-(define gcode-input->token
-  (lexer [(eof) (token-EOF)]
+(define import-gcode
+  (lexer [(eof) null]
          ;; Recursive call effectively skips whitespace
-         [whitespace (gcode-input->token input-port)]
-         [(union #\G #\M #\S #\F #\X #\Y #\Z #\I #\J #\K
-                 #\g #\m #\s #\f #\x #\y #\z #\i #\j #\k)
-          (token-CODE-LETTER lexeme)]
-         [numeric
-          (token-CODE-NUMBER (string->number lexeme))]))
-
-;; -------------------- PARSING
-
-;; Consumes a generator that produces tokens, and parses them into
-;; code?
-(define tokens->internal-gcode
-    (parser
-     (tokens code-tokens empty-tokens)
-     (start code-list)
-     (end EOF)
-     (error (lambda (tok-ok? tok-name tok-value)
-              (print "Error")))
-     (grammar (code-list
-               ((code) (list $1))
-               ((code code-list) (cons $1 $2)))
-              (code
-               ((CODE-LETTER CODE-NUMBER) (code $1 $2))))))
-
-;; High-level parsing function consuming an input-port? with G-Code and producing
-;; an equivalent list of code?.
-(define (import-gcode input)
-  (tokens->internal-gcode (lambda () (gcode-input->token input))))
+         [whitespace (import-gcode input-port)]
+         [(re-seq gcode-letter (re-* whitespace) gcode-number)
+          (cons (code-str->code lexeme) (import-gcode input-port))]))
